82a705201401ecd0356dad807d157b9c0452f932
Rayan Encarnacion <rayanbalbuena@gmail.com>
2021-09-24 15:49:01 -0400

    Menu changes its display one second after page load

--- jsModules/overlayMenu.js
+++ jsModules/overlayMenu.js
@@ -11,11 +11,10 @@ const menuTransition = () =>
     : (overlayMenu.style.transform = '');
 
 export default function () {
-  setTimeout(() => (overlayMenu.style.display = 'block'), 2000);
+  setTimeout(() => (overlayMenu.style.display = 'block'), 1000);
   header.addEventListener('click', e => {
-    // If the target contains a trigger class show/hide the menu
-    if (menuClasses.some(className => e.target.classList.contains(className))) {
-      menuTransition();
-    } else return;
+    menuClasses.some(className => e.target.classList.contains(className))
+      ? menuTransition()
+      : '';
   });
 }
