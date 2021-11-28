596eec86a63632cfebd83670df2d23af16478e1a
Rayan Encarnacion <rayanbalbuena@gmail.com>
2021-11-28 15:42:50 -0400

    Display none window.width over 1024px

--- jsModules/overlayMenu.js
+++ jsModules/overlayMenu.js
@@ -3,23 +3,17 @@ const overlayMenu = document.querySelector('.menu-overlay');
 // Classes that trigger the menu to show/hide
 const menuClasses = ['fas', 'far', 'menu-btn', 'menu-link'];
 
-const menuTransition = () =>
-  // if is visible, hide it and viceversa
-  overlayMenu.style.transform === `translateX(105%)` ||
-  overlayMenu.style.transform === ''
-    ? (overlayMenu.style.transform = 'translateX(0)')
-    : (overlayMenu.style.transform = '');
-
-const toggleMenu = () => {
-  overlayMenu.classList.toggle('active');
-};
+const menuTransition = () => overlayMenu.classList.toggle('active');
 
 export default function () {
-  setTimeout(() => (overlayMenu.style.display = 'block'), 1000);
-
   header.addEventListener('click', e => {
     menuClasses.some(className => e.target.classList.contains(className))
       ? menuTransition()
       : '';
   });
+
+  if (window.innerWidth <= 1024)
+    setTimeout(() => {
+      overlayMenu.style.display = 'block';
+    }, 1000);
 }
